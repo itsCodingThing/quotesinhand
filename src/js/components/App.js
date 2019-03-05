8f82ae04d0b236c743aa8cf61fbc0c0e78ead151
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2019-03-05 18:14:22 +0530

    adding renderQuote component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,20 +1,71 @@
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+// url: `http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=${n}`
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+async function getQuotes(num) {
+  let qoute;
+  let n = getRandomInt(num);
+
+  qoute = await axios
+    .get(
+      `http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=${n}`
+    )
+    .then(res => {
+      let { data } = res;
+      if (data.length > 1) {
+        let randomQuote = getRandomInt(data.length);
+        return {
+          title: data[randomQuote].title,
+          content: data[randomQuote].content
+        };
+      } else {
+        return {
+          title: data[0].title,
+          content: data[0].content
+        };
+      }
+    });
+
+  return qoute;
+}
+
+let RenderQuotes = qoute => {
+  console.log(qoute.content);
+  console.log(qoute.title);
+
+  return (
+    <Fragment>
+      {qoute.content}
+      <footer className="blockquote-footer" id="qoute-title">
+        {qoute.title}
+      </footer>
+    </Fragment>
+  );
+};
 
 export default () => {
+  let [qoute, setQuote] = useState({});
+
+  function changeQoute() {
+    getQuotes(10).then(res => {
+      setQuote({ ...res });
+    });
+  }
+
   return (
     <div className="container d-flex align-items-center justify-content-center">
       <div className="wrapper text-center">
         <h1 className="text-center">Quotes in hands</h1>
         <blockquote className="blockquote text-right">
-          <p className="mb-0" id="qoute-content">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-            posuere erat a ante."
-          </p>
-          <footer className="blockquote-footer" id="qoute-title">
-            Source Title
-          </footer>
+          <RenderQuotes {...qoute} />
         </blockquote>
-        <button className="btn btn-primary">Generate</button>
+        <button className="btn btn-primary" onClick={changeQoute}>
+          Generate
+        </button>
       </div>
     </div>
   );
