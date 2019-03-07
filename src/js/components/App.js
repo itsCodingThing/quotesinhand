7808024bcaf3e3c632ebc8e58fad156ec6eee6a2
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2019-03-07 21:55:22 +0530

    changeing the endpoint

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -6,10 +6,12 @@ import { RenderFooter, RenderQuotes } from "./Render";
 async function getQuotes() {
   let quote;
 
-  quote = await axios.get("https://node-quote.herokuapp.com/").then(res => {
-    let { data } = res;
-    return data;
-  });
+  quote = await axios
+    .get("https://node-quote.herokuapp.com/quote/")
+    .then(res => {
+      let { data } = res;
+      return data;
+    });
 
   return quote;
 }
