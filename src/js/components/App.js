7a18ccc317f701c8d02da1d7117af480265b44ac
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2019-03-11 20:04:07 +0530

    adding footer

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -7,6 +7,21 @@ export default class App extends React.Component {
       <Container className="d-flex flex-column">
         <h1 className="header text-center">Quotes in Hands</h1>
         <QuoteGen />
+        <footer className="mt-auto text-center">
+          <div className="innerbox">
+            <p>
+              created by{" "}
+              <a href="https://itsCodingThing.now.sh/" target="__blank">
+                itsCodingThing
+              </a>{" "}
+              powered by{" "}
+              <a href="https://quotesondesign.com/" target="__blank">
+                QuotesOnDesign
+              </a>
+              .
+            </p>
+          </div>
+        </footer>
         <style jsx>{`
           .header {
             font-family: "Playfair Display", serif;
