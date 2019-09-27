c2748178b4d8edfe277e47957f018e9265173ce4
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2019-09-27 21:06:39 +0530

    adding error handling in getQuotes method

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 async function getQuotes() {
-  let quote = await axios.get("https://node-quote.herokuapp.com/quote/").then((res) => {
-    let { data } = res;
-    return data;
-  });
-
-  return quote;
+  try {
+    let quote = await axios.get("https://node-quote.herokuapp.com/quote/");
+    return quote.data;
+  } catch (error) {
+    return {
+      title: "Ooopss!!!",
+      content: "no quotes available at this moment 🙏",
+    };
+  }
 }
 
 export { getQuotes };
