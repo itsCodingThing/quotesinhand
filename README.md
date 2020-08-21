f87373c58fe3b4ac846a60ad480f2686cd6efb51
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2020-08-21 21:07:26 +0530

    replacing bundler to parcel and restructuring the project

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-## A random qoute generator using react
-
-### API used QUOTESONDESIGN <a>https://quotesondesign.com/</a>
+## A website for random qoutes
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
-import "./style.css";
-import icon from "./public/itsCodingThing_logo.ico";
 
 let root = document.getElementById("root");
 
